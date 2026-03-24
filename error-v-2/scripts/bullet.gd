211be2d81c8c097cda885145d97a7e8f255e1bdb
Jeremy <jerrytimi.ayeni@gmail.com>
2026-03-24 11:41:05 +0000

    Add Kenney desert shooter asset pack
    Add Kenney Desert Shooter Pack v1.0 assets under error-v-2/Assets. Includes PNG art (enemies, interface tiles, tilemaps, packed textures, tilesheet), Unity .import metadata files, and the License.txt. These assets populate the project's tile and sprite resources for levels and UI.

--- error-v-2/scripts/bullet.gd
+++ error-v-2/scripts/bullet.gd
@@ -4,14 +4,9 @@ const SPEED = 600.0
 
 var direction = Vector2.ZERO
 
-func _ready():
-	body_entered.connect(_on_body_entered)
 
 func _physics_process(delta):
 	position += direction * SPEED * delta
-
-func _on_body_entered(body):
-	queue_free()
 
 func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
